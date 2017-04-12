e0fab9299321fcbff6991dd78b14d51f0128d67b
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-04-12 17:08:14 +0300

    regexp navbar

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -5,7 +5,7 @@
         navbarNode.addEventListener('click', function (event) {
             event.preventDefault();
             if (event.target.className === 'menu__link') {
-                router.renderComponent(event.target.textContent.replace(/[^a-zA-Z]+/g, ""));
+                router.renderComponent(event.target.textContent.replace(/[\W]+/g, ""));
             }
         });
     }
