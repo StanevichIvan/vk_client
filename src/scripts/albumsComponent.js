5b3536bd39972ef85d6edd173daf51cadb9c4539
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-04-18 19:10:02 +0300

    albums component rendering

---
+++ src/scripts/albumsComponent.js
@@ -0,0 +1,62 @@
+(function () {
+
+    const vkService = window.app.xhrService;
+
+    function AlbumsComponent(id) {
+
+        this.activeRequest = {};
+        this.container = document.getElementById('router-outlet');
+
+        this.component = document.createElement('div');
+        this.component.classList.add('albums');
+        this.albums = [];
+        this.id = id;
+
+        this.loadAlbums = () => {
+            vkService.getAlbums(this.activeRequest, this.id)
+                .then((res) => {
+                    this.albums = res;
+                    this.render();
+                }).catch((err) => {
+                // console.log(err);
+            });
+        };
+
+        this.loadAlbums();
+    }
+
+    AlbumsComponent.prototype.destroy = function () {
+        this.component.innerHTML = '';
+    };
+
+    AlbumsComponent.prototype.render = function () {
+        this.component.appendChild(this.createAlbumTumb(this.albums));
+        this.container.appendChild(this.component);
+    };
+
+    AlbumsComponent.prototype.createAlbumTumb = function(albumList) {
+        let fragment = document.createDocumentFragment();
+
+        for (let i = 0; i < albumList.length; i++) {
+            fragment.appendChild(this.renderThumb(albumList[i]));
+        }
+
+        return fragment;
+    };
+
+    AlbumsComponent.prototype.renderThumb = function(obj) {
+        let div = document.createElement('div');
+        div.classList.add('albums__tumb');
+        div.dataset.id = obj.id;
+        div.style.backgroundImage = `url('${obj.coverSrc}')`;
+        div.innerHTML = `<p class="albums__tumb-text">${obj.title}</p>`;
+
+        div.addEventListener('click', (e)=> {
+            window.app.router.renderComponent('photos', {userId: this.id, albumId: obj.id});
+        });
+
+        return div;
+    };
+
+    app.albumsComponent = AlbumsComponent;
+})();
