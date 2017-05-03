64261630009fe709999d8447732aa72252d9a5ae
Ivan Stanevich <vanya.stanevich@gmail.com>
2017-05-03 18:53:00 +0300

    cors added

--- server.js
+++ server.js
@@ -16,6 +16,11 @@
         bodyParser = require('body-parser');
 
     var server = http.createServer(function (req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+
         var urlData = url.parse(req.url),
             theUrl = urlData.pathname,
             someFileName = path.join(process.cwd(), theUrl);
